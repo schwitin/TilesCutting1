60f9636d194d3cb46f7a9fdbfbd776a072a3c160
schwitin <wladimir.schwitin@gmail.com>
2019-09-22 08:33:26 +0200

    Export nach HTML5 hinzugefügt

--- Model/ZiegelTypDAO.gd
+++ Model/ZiegelTypDAO.gd
@@ -19,7 +19,23 @@ func load_ziegel_typen():
 	file.open("res://Resources/ZiegelTyp.json", file.READ)
 	var result_json = JSON.parse(file.get_as_text())
 	file.close()
+	var dict = {
+		"Metaden":{},
+		"ZiegelTypList":[
+			{
+				"Name": "Muster",
+				"Hersteller": "Unbekannt",
+				"Laenge" : "482",
+				"Breite" : "292",
+				"VersatzY": "16",
+				"DecklaengeMin" : "410",
+				"DecklaengeMax" : "430",
+				"Deckbreite" : "241"
+			}
+		]
+	}
 	if result_json.error == OK:
-		var dict = result_json.result
-		for typ in dict.ZiegelTypList :
-			ziegelTypen.append(ziegelTypClass.new(typ))
+		dict = result_json.result
+		
+	for typ in dict.ZiegelTypList :
+		ziegelTypen.append(ziegelTypClass.new(typ))
